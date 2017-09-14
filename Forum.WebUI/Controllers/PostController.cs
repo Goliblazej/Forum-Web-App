3fbaaf7f148dc5b82ed76efd43dc3801441b9427
Goliblazej <blazejgolinski@gmail.com>
2017-09-14 01:04:25 +0100

    Domain Model Entity Framework

---
+++ Forum.WebUI/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forum.Domain.Abstract;
+using Forum.Domain.Entities;
+
+namespace Forum.WebUI.Controllers
+{
+    public class PostController : Controller
+    {
+        private IPostRepository repository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            this.repository = postRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Posts);
+        }
+    }
+}
--- Forum.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ Forum.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,29 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using Forum.Domain.Abstract;
+using Forum.Domain.Entities;
+using Forum.Domain.Concrete;
 
 namespace Forum.WebUI.Infrastructure
 {
     public class NinjectDependencyResolver : IDependencyResolver
     {
         private IKernel kernel;
+
         public NinjectDependencyResolver(IKernel kernelParam)
         {
             kernel = kernelParam;
             AddBindings();
         }
+
         public object GetService(Type serviceType)
         {
             return kernel.TryGet(serviceType);
         }
+
         public IEnumerable<object> GetServices(Type serviceType)
         {
             return kernel.GetAll(serviceType);
         }
+
         private void AddBindings()
         {
-            // tu umieść dodatkowe powiązania
+            kernel.Bind<IPostRepository>().To<EFPostRepository>();
+            kernel.Bind<ITopicRepository>().To<EFTopicRepository>();
+            kernel.Bind<IUserRepository>().To<EFUserRepository>();
         }
     }
 }
